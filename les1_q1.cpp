6874c373b21fc533247b77f443259c385073f832
Konstantin Kylikov <koost99@mail.ru>
2022-11-09 21:47:05 +0300

    Create les1_q1.cpp

---
+++ les1_q1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Power
+{
+private:
+    float a = 2;
+    float b = 2;
+
+public:
+    void set(char x, float dat) {
+        switch (x)
+        {
+        case('a'):
+            a = dat;
+            break;
+        case('b'):
+            b = dat;
+            break;
+        default:
+            cout << "not" << endl;
+            break;
+        }
+    }
+   
+    void calculate() {
+        cout << pow(a, b) << endl;
+    }
+
+};
+
+void main()
+{
+    setlocale(LC_ALL, "Ru");
+    Power abc;
+    cout << "введите название переменной (в данной программе a или b). "<< endl <<"или введите 0 в качестве имя переменной для выхода из программы" << endl << endl;
+    char a;
+    float b;
+    while (true)
+    {
+        cout << "Введите имя переменной, которую хотите изменить или 0" << endl;
+        cin >> a;
+        if (a == 48)  break; 
+        cout << "Введите введите число" << endl;
+        cin >> b;
+        abc.set(a, b);
+        abc.calculate();
+    } 
+    
+        
+    
+}
+
